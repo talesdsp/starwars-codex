ed0590e3dd99b46c25e9755c4b5bfe231a5bf1e4
Tales Pereira <talesdsp@gmail.com>
2020-02-19 13:14:43 -0300

    moved to sibling file

--- src/Routes/Characters/Char.js
+++ src/Routes/Characters/Char.js
@@ -1,13 +1,6 @@
-import React from "react";
-import * as S from "../../styles/styled";
 
 export default function Char({char}) {
   return (
-    <S.Item>
-      <h1>{char.name}</h1>
-      <p>{char.age}</p>
-      <p>{char.gender}</p>
-      <p>{char.mass}</p>
-    </S.Item>
+
   );
 }
