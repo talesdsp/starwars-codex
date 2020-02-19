faaa32be689e26c350b3da2c98bc291a4c1880e2
Tales Pereira <talesdsp@gmail.com>
2020-02-19 13:12:57 -0300

    add stars

---
+++ src/styles/styled/Stars.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+import "../css/bg.css";
+
+const Stars = () => (
+  <>
+    <Star className="layer1" />
+    <Star className="layer1 a" />
+    <Star className="layer2" />
+    <Star className="layer2 a" />
+    <Star className="layer3" />
+    <Star className="layer3 a" />
+  </>
+);
+
+export default Stars;
+
+const Star = styled.div`
+  position: fixed;
+  top: -100vh;
+  border-radius: 50%;
+  background-image: transparent;
+  animation-name: fall;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+`;
