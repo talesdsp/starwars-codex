b12a3fdd346a966aa141e1e99f25f646460bfe6e
Tales Pereira <talesdsp@gmail.com>
2020-02-19 13:14:04 -0300

    miscellaneous

--- src/styles/styled/index.js
+++ src/styles/styled/index.js
@@ -1,5 +1,5 @@
 import {Link} from "react-router-dom";
-import styled from "styled-components";
+import styled, {css, keyframes} from "styled-components";
 
 export const Window = styled.main`
   position: relative;
@@ -9,21 +9,17 @@ export const Window = styled.main`
   align-items: center;
   justify-content: center;
   height: 100vh;
-  h1 {
-    font-size: 8rem;
-    font-weight: 900;
-    text-shadow: 0 0 1px #ff0;
-    font-family: sans-serif;
-  }
 `;
 
 export const Button = styled(Link)`
-  font-size: 2.4rem;
+  font-size: 2rem;
   text-decoration: none;
+  text-transform: uppercase;
   color: #fff;
   font-weight: bold;
   background-color: #323535;
   width: 100%;
+  max-width: 40vw;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -43,6 +39,13 @@ export const Button = styled(Link)`
       #660a2d 100%
     );
   }
+
+  ${(props) =>
+    props.disabled &
+    css`
+      pointer-events: none;
+      cursor: not-allowed;
+    `}
 `;
 
 export const List = styled.ul`
@@ -53,7 +56,7 @@ export const List = styled.ul`
   align-items: center;
   justify-content: center;
   width: 40%;
-  height: 40%;
+  min-height: max-content;
   background-color: transparent;
 `;
 export const Item = styled.li`
@@ -72,4 +75,63 @@ export const Item = styled.li`
   p {
     font-size: 1.6rem;
   }
+`;
+
+export const Role = styled.h1`
+  font-size: 4rem;
+  color: #fff;
+  width: 100%;
+  text-align: center;
+  background-color: #03121a;
+  padding: 4rem 0;
+`;
+
+const fadeIn = keyframes`
+from{
+  opacity: 0;
+  transform: translate3d(0, 3rem, 0);
+}
+to{ 
+  opacity: 1;
+  transform: translate3d(0, 0, 0);
+  };
+`;
+
+export const Card = styled.div`
+  display: flex;
+  animation: ${fadeIn} 1s ease;
+`;
+
+export const Modal = styled.div`
+  animation: ${fadeIn} 1s ease;
+`;
+
+export const PopUp = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  margin: auto;
+  padding: 1rem 0;
+  width: 100%;
+  z-index: 2;
+  background-image: linear-gradient(90deg, rgba(0, 0, 0, 0) 0%, #444 50%, rgba(0, 0, 0, 0) 100%);
+  animation: ${fadeIn} 1s ease;
+`;
+
+export const Page = styled.div`
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  color: red;
+  font-size: 1.6rem;
+  text-decoration: underline;
+`;
+
+export const Numbers = styled.span`
+  padding: 0.5rem;
+  margin: 0 0.5rem;
 `;
