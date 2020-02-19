3f94c34464379f9f650c98e4a46361c14fe4d10b
Tales Pereira <talesdsp@gmail.com>
2020-02-19 13:16:40 -0300

    initial layout

--- src/Routes/Characters/index.js
+++ src/Routes/Characters/index.js
@@ -2,46 +2,57 @@ import React, {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {dataCreators} from "../../redux/ducks/data";
 import {extractPathFromUrl} from "../../services/api";
+import * as S from "../../styles/styled";
 import Spinner from "../../styles/styled/Spinner";
-import Char from "./Char";
 
 const Characters = (props) => {
   let theme = "people";
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(dataCreators.triggerLoading());
     dispatch(dataCreators.get(theme));
   }, [dispatch, theme]);
 
-  const goto = (url) => {
+  const goto = (url, e) => {
+    if (!url) return;
+    dispatch(dataCreators.triggerLoading());
     dispatch(dataCreators.get(extractPathFromUrl(url)));
   };
 
-  const [{results, isLoading, count}] = useSelector((state) => [state.data]);
-  console.log(results);
+  const [{results, isLoading, count, next, previous}] = useSelector((state) => [state.data]);
   return (
     <>
+      <S.Role>Characters</S.Role>
       {isLoading ? <Spinner /> : ""}
 
-      {results && results.map((v) => <Char key={v.name} char={v} />)}
-
-      <button disabled={results && !results.previous} onClick={() => goto(results.previous)}>
-        Previous
-      </button>
-
-      {count &&
-        [...Array(Math.trunc(count / 10) + (count % 10 > 0 && 1))].map((v, i) => (
-          <div
-            style={{cursor: "pointer", color: "red", fontSize: "2rem", textDecoration: "underline"}}
-            onClick={() => goto("https://swapi.co/api/people/?page=" + i + 1)}
-          >
-            {i + 1}
-          </div>
+      {results &&
+        results.map((v) => (
+          <S.Item key={v.name}>
+            <S.Button to="">
+              <h1>{v.name}</h1>
+            </S.Button>
+            <S.Modal></S.Modal>
+          </S.Item>
         ))}
 
-      <button disabled={results && !results.next} onClick={() => goto(results.next)}>
+      <S.Button disabled={previous === null} onClick={() => goto(previous)}>
+        Previous
+      </S.Button>
+
+      <S.PopUp></S.PopUp>
+      <S.Page>
+        {count &&
+          [...Array(Math.trunc(count / 10) + (count % 10 > 0 && 1))].map((v, i) => (
+            <S.Numbers onClick={(e) => goto(`https://swapi.co/api/people/?page=${i + 1}`, e)}>
+              {i + 1}
+            </S.Numbers>
+          ))}
+      </S.Page>
+
+      <S.Button disabled={next === null} onClick={(e) => goto(next, e)}>
         Next
-      </button>
+      </S.Button>
     </>
   );
 };
