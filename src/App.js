a382f33302b46bbaf8db2e940b86019319e816ca
Tales Pereira <talesdsp@gmail.com>
2020-02-19 19:15:21 -0300

    add navigation controls

--- src/App.js
+++ src/App.js
@@ -6,18 +6,95 @@ import Home from "./Routes/Home";
 import Stars from "./styles/styled/Stars";
 
 function App() {
+  let isLoaded = false;
+  window.onload = () => {
+    isLoaded = true;
+  };
+  function prepareNavigation(isLoaded) {
+    let actual;
+    let btn;
+    let intervalId;
+    intervalId = setInterval(startNavigation, 200);
+
+    function startNavigation(isLoaded) {
+      if (isLoaded === false) return;
+      clearInterval(intervalId);
+      btn = [];
+      actual = 0;
+      btn = document.querySelectorAll("button");
+
+      if (btn.length === 0) return prepareNavigation(isLoaded);
+
+      document.addEventListener("keyup", applyFocus);
+      btn.forEach((a) => a.addEventListener("mouseenter", applyFocus));
+      btn.forEach((a) => a.addEventListener("mouseleave", applyFocus));
+      btn[0].focus();
+      applyFocus();
+    }
+
+    function applyFocus(e = {}) {
+      if ((e.keyCode === 38) & (actual > 0)) {
+        actual -= 1;
+        btn[actual].focus();
+      } else if ((e.keyCode === 40) & (actual < btn.length - 1)) {
+        actual += 1;
+        btn[actual].focus();
+      } else if (e.target) {
+        e.target.focus();
+      }
+      if (document.activeElement.nextSibling !== null) {
+        btn.forEach((v) => {
+          v.nextSibling && v.nextSibling.removeAttribute("data-openpreview");
+        });
+        document.activeElement.nextSibling.setAttribute("data-openpreview", true);
+      }
+    }
+  }
+
   return (
     <BrowserRouter>
       <Stars />
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/categories" component={Categories} />
-        <Route exact path="/categories/characters" component={Characters} />
-        <Route exact path="/categories/starships" component={Characters} />
-        <Route exact path="/categories/films" component={Characters} />
-        <Route exact path="/categories/vehicles" component={Characters} />
-        <Route exact path="/categories/species" component={Characters} />
-        <Route exact path="/categories/planets" component={Characters} />
+        <Route
+          exact
+          path="/"
+          render={(props) => <Home {...props} fn={prepareNavigation(isLoaded)} />}
+        />
+        <Route
+          exact
+          path="/categories"
+          render={(props) => <Categories {...props} fn={prepareNavigation(isLoaded)} />}
+        />
+        <Route
+          exact
+          path="/categories/characters"
+          render={(props) => <Characters {...props} fn={prepareNavigation(isLoaded)} />}
+        />
+        <Route
+          exact
+          path="/categories/starships"
+          render={(props) => <Characters {...props} fn={prepareNavigation(isLoaded)} />}
+        />
+        <Route
+          exact
+          path="/categories/films"
+          render={(props) => <Characters {...props} fn={prepareNavigation(isLoaded)} />}
+        />
+        <Route
+          exact
+          path="/categories/vehicles"
+          render={(props) => <Characters {...props} fn={prepareNavigation(isLoaded)} />}
+        />
+        <Route
+          exact
+          path="/categories/species"
+          render={(props) => <Characters {...props} fn={prepareNavigation(isLoaded)} />}
+        />
+        <Route
+          exact
+          path="/categories/planets"
+          render={(props) => <Characters {...props} fn={prepareNavigation(isLoaded)} />}
+        />
       </Switch>
     </BrowserRouter>
   );
