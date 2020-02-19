2fb234af143709b419d6936166a762f552bd39e7
Tales Pereira <talesdsp@gmail.com>
2020-02-19 19:31:59 -0300

    add watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
-var autoprefixer = require("autoprefixer");
 
 gulp.task("sass", function() {
   return gulp
     .src("src/styles/scss/**/*.scss")
     .pipe(sass({outputStyle: "compressed"}))
     .pipe(gulp.dest("src/styles/css"));
+});
+
+gulp.task("watch", function() {
+  gulp.watch("src/styles/scss/**/*.scss", gulp.parallel("sass"));
 });
