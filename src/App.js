9cc5c1b311bb233439f663ee5ba19d42b4e29abd
Tales Pereira <talesdsp@gmail.com>
2020-02-19 00:39:25 -0300

    categories in between

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Categories from "./Routes/Categories";
 import Characters from "./Routes/Characters";
 import Home from "./Routes/Home";
 import Stars from "./styles/styled/Stars";
@@ -10,12 +11,13 @@ function App() {
       <Stars />
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/characters" component={Characters} />
-        <Route exact path="/starships" component={Characters} />
-        <Route exact path="/films" component={Characters} />
-        <Route exact path="/vehicles" component={Characters} />
-        <Route exact path="/species" component={Characters} />
-        <Route exact path="/planets" component={Characters} />
+        <Route exact path="/categories" component={Categories} />
+        <Route exact path="/categories/characters" component={Characters} />
+        <Route exact path="/categories/starships" component={Characters} />
+        <Route exact path="/categories/films" component={Characters} />
+        <Route exact path="/categories/vehicles" component={Characters} />
+        <Route exact path="/categories/species" component={Characters} />
+        <Route exact path="/categories/planets" component={Characters} />
       </Switch>
     </BrowserRouter>
   );
