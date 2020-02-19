3407ea7e2cbe1264db25b71923d929bc5b317488
Tales Pereira <talesdsp@gmail.com>
2020-02-19 00:38:46 -0300

    Home added

--- src/Routes/Home/index.js
+++ src/Routes/Home/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import logo from "../../logo.svg";
+import * as S from "../../styles/styled";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Star Wars</h1>
-      <Link to="/characters">click me</Link>
-    </div>
+    <S.Window>
+      <img src={logo} alt="Star Wars" />
+      <S.Button to="/categories">Start</S.Button>
+    </S.Window>
   );
 }
