20427364dcd8df0699c5123f228f4d1d09a24ecb
Tales Pereira <talesdsp@gmail.com>
2020-02-20 02:26:56 -0300

    added species

---
+++ src/components/Species.js
@@ -0,0 +1,27 @@
+import React from "react";
+import * as S from "../styles/styled/index";
+
+const Species = ({results, closeModal}) => {
+  return (
+    <>
+      {results.map((v, i) => (
+        <S.Item key={i}>
+          <S.Button>
+            <h1>{v.name}</h1>
+          </S.Button>
+          <S.Modal>
+            <S.X onClick={closeModal} />
+            <h1>{v.name}</h1>
+            <p>classification: {v.classification}</p>
+            <p>designation: {v.designation}</p>
+            <p>language: {v.language}</p>
+            <p>average_height: {v.average_height}</p>
+            <p>average_lifespan: {v.average_lifespan}</p>
+          </S.Modal>
+        </S.Item>
+      ))}
+    </>
+  );
+};
+
+export default Species;
